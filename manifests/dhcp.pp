875a698dcc8964465e7888c5074d2b33a66ccce6
Eric Cohen <eirc.eirc@gmail.com>
2013-09-28 02:16:11 +0300

    Quoted file modes.

--- manifests/dhcp.pp
+++ manifests/dhcp.pp
@@ -2,7 +2,7 @@ define interfaces::dhcp ($interface) {
   file { '/etc/network/interfaces':
     owner   => root,
     group   => root,
-    mode    => 644,
+    mode    => '0644',
     content => template('interfaces/interfaces-dhcp'),
   }
 }
--- manifests/static.pp
+++ manifests/static.pp
@@ -2,7 +2,7 @@ define interfaces::static ($interface, $ip, $netmask, $broadcast, $gateway) {
   file { '/etc/network/interfaces':
     owner   => root,
     group   => root,
-    mode    => 644,
+    mode    => '0644',
     content => template('interfaces/interfaces-static'),
   }
 }
